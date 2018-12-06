93d49f640596f044d2e90afc2918fbb24397da76
Hamza Jibran <35891072+hamzajb95@users.noreply.github.com>
2018-12-06 11:13:57 +0500

    Update liveSearchserver.py

--- liveSearchserver.py
+++ liveSearchserver.py
@@ -5,7 +5,7 @@ import unicodecsv as csv
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
-import time
+import bs4 as bs
 import datetime
 import time
 
